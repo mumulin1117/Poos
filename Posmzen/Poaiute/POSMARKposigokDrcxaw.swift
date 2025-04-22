0a28428eafc245158e423113274efc764a126970
mumulin1117 <mumulin1117@163.com>
2025-04-22 18:23:24 +0800

    Update POSMARKposigokDrcxaw.swift

--- Posmzen/Poaiute/POSMARKposigokDrcxaw.swift
+++ Posmzen/Poaiute/POSMARKposigokDrcxaw.swift
@@ -219,11 +219,13 @@ class POSMARKposigokDrcxaw: UIViewController, WKNavigationDelegate, WKUIDelegate
     // ... (保持原有委托方法不变)
     
     func FlashFable() {
-        poseWebView?.isHidden = false
-        SVProgressHUD.dismiss()
-        
-        // 添加摄影闪光效果
-        animateFlashEffect()
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 1, execute: DispatchWorkItem(block: {
+            self.poseWebView?.isHidden = false
+            SVProgressHUD.dismiss()
+            
+            // 添加摄影闪光效果
+            self.animateFlashEffect()
+        }))
     }
     
     private func animateFlashEffect() {
