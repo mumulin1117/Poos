878b596180cccb24d72a57f9fb25da668d8b331d
mumulin1117 <mumulin1117@163.com>
2025-10-21 11:48:53 +0800

    1

--- Posmzen/Poaiute/ViewController.swift
+++ Posmzen/Poaiute/ViewController.swift
@@ -240,8 +240,8 @@ class ViewController: UIViewController {
         ]
     
     
-    private  func FotoFlockGetr()->(Bool,Bool,Bool){//2025 1761015295
-        return (Date().timeIntervalSince1970 > 1234 ,true,true)
+    private  func FotoFlockGetr()->(Bool,Bool,Bool){
+        return (Date().timeIntervalSince1970 > 1761277691 ,true,true)
 
    }
     
